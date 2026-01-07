980790cead1199459d46b077446dbed433c51d31
苏渡苇 <>
2026-01-07 23:29:48 +0800

    增加demo模块框架

---
+++ demo-app/src/main/java/io/github/iweidujiang/springinsight/demo/DemoAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.iweidujiang.springinsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+
+}
---
+++ demo-app/src/test/java/io/github/iweidujiang/springinsight/demo/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iweidujiang.springinsight.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
